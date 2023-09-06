6257ffc0381bb395c81e363f7b4cc561f060f6d3
Facundo Fierro <fierrof_47@hotmail.com>
2023-09-06 12:05:06 +0200

    Simplified everithing for testing

--- RuneGenerator/utils/paths.py
+++
@@ -1,6 +0,0 @@
-import os
-
-DIVNE_DIR = "D:\Apps\Modding Tools\Baldurs Gate 3\ExportTool\Tools\divine.exe"
-RUNES_JSON_DIR = os.path.join(os.path.dirname(__file__), '..', 'runes.json')
-SPELL_LISTS_DIR = os.path.join(os.path.dirname(__file__), '..', 'SpellLists.lsx')
-SHOUTS_DIR = os.path.join(r'D:\Projects\Mods\Baldurs Gate 3\FFT_WizardTraditions_Runesmith\Public\FFT_WizardTraditions_Runesmith\Stats\Generated\Data\Shout.txt')
